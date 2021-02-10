055a5c7fff65ce8729b39b06f660f1a82bd7bda9
jeikabu <jeikabu@users.noreply.github.com>
2021-02-10 18:07:18 +0200

    ESP IDF v4.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,31 @@
-use std::{env, path::PathBuf, process, sync::mpsc, thread, time};
+use std::{env, io, path, process, sync::mpsc, thread, time};
 
 /// Runs make/cmake to ensure native components are built.
 /// Even once built this will add a few seconds to your build time.
-pub fn build_native() -> std::thread::Result<()> {
+pub fn build_native() -> thread::Result<()> {
     let start = time::Instant::now();
     //let spinner = indicatif::ProgressBar::new_spinner();
     let (tx, rx) = mpsc::sync_channel(1);
     let make = thread::spawn(move || {
         let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
         if cfg!(feature = "build_idf") {
-            process::Command::new("idf.py")
+            let res = process::Command::new("idf.py")
                 .arg("build")
-                .current_dir(&std::path::Path::new(&manifest_dir))
-                .status()
-                .unwrap();
+                .current_dir(&path::Path::new(&manifest_dir))
+                .status();
+            match res {
+                Ok(_status) => {},
+                // If `idf.py` is not in $PATH:
+                //
+                // thread '<unnamed>' panicked at 'called `Result::unwrap()` on an `Err` value: Os { code: 2, kind: NotFound, message: "No such file or directory" }', /home/matrix-io/esp_idf_build/src/lib.rs:12:13
+                // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+                // thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: Any', build.rs:4:5
+                Err(error) if error.kind() == io::ErrorKind::NotFound => panic!("Not found. `source $IDF_PATH/export.sh`?"),
+                Err(error) => panic!("{}", error),
+            }
         } else if cfg!(feature = "build_make") {
             process::Command::new("make")
-                .current_dir(&std::path::Path::new(&manifest_dir))
+                .current_dir(&path::Path::new(&manifest_dir))
                 .status()
                 .unwrap();
         } else {
@@ -33,11 +42,11 @@ pub fn build_native() -> std::thread::Result<()> {
         // NOTE: no point in printing anything because it's buffered and no way to flush it.
         //println!("cargo:warning=Building...");
         //spinner.tick();
-        std::thread::sleep_ms(1000);
+        thread::sleep(time::Duration::from_millis(1000));
     }
     let ret = make.join();
     //spinner.finish();
-    println!("cargo:warning=Build time {:?}", start.elapsed());
+    println!("cargo:warning=ESP-IDF build_native() time {:?}", start.elapsed());
     ret
 }
 
@@ -46,9 +55,9 @@ pub fn print_link_search() {
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     if target_arch == "xtensa" {
         let esp_idf =
-            PathBuf::from(env::var("IDF_PATH").expect("IDF_PATH environment variable must be set"));
+            path::PathBuf::from(env::var("IDF_PATH").expect("IDF_PATH environment variable must be set"));
         // Folder containing `build/` output after running `make menuconfig && make`
-        let build_subdir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("build");
+        let build_subdir = path::PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("build");
         if glob::glob(&build_subdir.join("*.bin").to_string_lossy())
             .unwrap()
             .next()
@@ -58,11 +67,16 @@ pub fn print_link_search() {
         }
 
         let build_dirs = [
+            // Directories when using `idf.py build`
+            "esp-idf/esp32",
+            "esp-idf/esp32/ld",
+
             "app_trace",
             "app_update",
             "asio",
             "bootloader_support",
             "bt",
+            "cbor",
             "coap",
             "console",
             "cxx",
@@ -75,15 +89,23 @@ pub fn print_link_search() {
             "esp_eth",
             "esp_event",
             "esp_gdbstub",
+            "esp_hid",
             "esp_http_client",
             "esp_http_server",
             "esp_https_ota",
+            "esp_https_server",
+            "esp_ipc",
             "esp_local_ctrl",
+            "esp_netif",
             "esp_ringbuf",
             "esp_rom",
+            "esp_serial_slave_link",
+            "esp_system",
+            "esp_timer",
             "esp_websocket_client",
             "esp_wifi",
             "espcoredump",
+            "ethernet",
             "expat",
             "fatfs",
             "freemodbus",
@@ -103,10 +125,12 @@ pub fn print_link_search() {
             "nghttp",
             "nvs_flash",
             "openssl",
+            "perfmon",
             "protobuf-c",
             "protocomm",
             "pthread",
             "sdmmc",
+            "smartconfig_ack",
             "soc",
             "spi_flash",
             "spiffs",
@@ -125,7 +149,7 @@ pub fn print_link_search() {
         let idf_components = [
             "esp32/ld",
             "esp_rom/esp32/ld",
-            "esp_wifi/lib_esp32",
+            "esp_wifi/lib/esp32",
             "xtensa/esp32",
         ]
         .iter()
@@ -139,9 +163,9 @@ pub fn print_link_search() {
 
 /// Writes script file that uses esptool.py to create an application image from
 /// the Rust ELF.
-pub fn esptool_write_script() -> std::io::Result<()> {
-    use std::io::Write;
-    let root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+pub fn esptool_write_script() -> io::Result<()> {
+    use io::Write;
+    let root = path::PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let mut file = std::fs::File::create(root.join("image.sh"))?;
 
     // Make it executable
